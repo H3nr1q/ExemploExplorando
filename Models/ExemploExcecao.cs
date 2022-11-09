c3c5e86b83b25ba35e6f826a8f9d80e78c40dc03
Casa <carlos_henrique.taqua@hotmail.com>
2022-11-08 22:43:38 -0300

    Praticando exceções genéricas e especificas

---
+++ Models/ExemploExcecao.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class ExemploExcecao
+    {
+        public void Metodo1(){
+            try
+            {
+                Metodo2();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exceção Tratada" +ex.Message);
+            }
+        }
+
+        public void Metodo2()
+        {
+            Metodo3();
+        }
+
+        public void Metodo3()
+        {
+            throw new Exception("Ocorreu uma exceção");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,63 +1,90 @@
 ﻿using ExemploExplorando.Models;
 using System.Globalization;
 
-Pessoa p1 = new("Carlos", "Silva");
-Pessoa p2 = new("Henrique", "Silva");
-Pessoa p3 = new("Henrique", "Mariano");
+// Pessoa p1 = new("Carlos", "Silva");
+// Pessoa p2 = new("Henrique", "Silva");
+// Pessoa p3 = new("Henrique", "Mariano");
 
-Curso cursoDeIngles = new();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
+// Curso cursoDeIngles = new();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
 
-cursoDeIngles.AdicionaAluno(p1);
-cursoDeIngles.AdicionaAluno(p2);
-cursoDeIngles.AdicionaAluno(p3);
-cursoDeIngles.ListarAlunos();
-cursoDeIngles.ObterQuantideAlunosMatriculados();
+// cursoDeIngles.AdicionaAluno(p1);
+// cursoDeIngles.AdicionaAluno(p2);
+// cursoDeIngles.AdicionaAluno(p3);
+// cursoDeIngles.ListarAlunos();
+// cursoDeIngles.ObterQuantideAlunosMatriculados();
 
-cursoDeIngles.RemoveAluno(p3);
-cursoDeIngles.ListarAlunos();
-
-
-decimal valorMonetario1 = 1054.23M;
-decimal valorMonetario2 = 3365.98M;
+// cursoDeIngles.RemoveAluno(p3);
+// cursoDeIngles.ListarAlunos();
 
 
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-Br");
-Console.WriteLine($"{valorMonetario1:C}");
-Console.WriteLine(valorMonetario1.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
-Console.WriteLine($"{valorMonetario2:C}");
+// decimal valorMonetario1 = 1054.23M;
+// decimal valorMonetario2 = 3365.98M;
 
 
-double porcentagem = .3254;
-
-Console.WriteLine(porcentagem.ToString("P"));
-
-int numero = 20221107;
-
-Console.WriteLine(numero.ToString("####-##-##"));
-
-DateTime data = DateTime.Now;
-int day = data.Day;
-
-Console.WriteLine($"{data}, {day} , {data.ToShortTimeString()}, {data.ToShortDateString()}");
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-Br");
+// Console.WriteLine($"{valorMonetario1:C}");
+// Console.WriteLine(valorMonetario1.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+// Console.WriteLine($"{valorMonetario2:C}");
 
 
-string dataString = "2022-07-170 18:00";
+// double porcentagem = .3254;
 
-DateTime.TryParseExact(dataString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date2);
-Console.WriteLine(date2);
+// Console.WriteLine(porcentagem.ToString("P"));
+
+// int numero = 20221107;
+
+// Console.WriteLine(numero.ToString("####-##-##"));
+
+// DateTime data = DateTime.Now;
+// int day = data.Day;
+
+// Console.WriteLine($"{data}, {day} , {data.ToShortTimeString()}, {data.ToShortDateString()}");
+
+
+// string dataString = "2022-07-170 18:00";
+
+// DateTime.TryParseExact(dataString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date2);
+// Console.WriteLine(date2);
 
 // DateTime date3 = DateTime.Parse(dataString);
 // Console.WriteLine(date3);
 
-bool sucess = DateTime.TryParseExact(dataString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date);
+// bool sucess = DateTime.TryParseExact(dataString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date);
 
-if (sucess)
+// if (sucess)
+// {
+//     Console.WriteLine($"Conversão com sucesso! Data: {date}");
+// }
+// else
+// {
+//     Console.WriteLine($"{dataString} não é uma data válida");
+// }
+
+try
 {
-    Console.WriteLine($"Conversão com sucesso! Data: {date}");
+    string[] linhas = File.ReadAllLines("Arquivos/AArquivoLeitura.txt");
+
+    foreach(string linha in linhas){
+        Console.WriteLine(linha);
+    }  
 }
-else
+catch (FileNotFoundException ex)
 {
-    Console.WriteLine($"{dataString} não é uma data válida");
+    Console.WriteLine($"Arquivo não encontrado. {ex.Message}");
 }
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Diretório não encontrado. {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+}
+finally{
+    Console.WriteLine("Chegou até aqui e finalizou!");
+}
+
+
+new ExemploExcecao().Metodo1();
